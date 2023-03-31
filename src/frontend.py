30ec368bae21a86f2ecbba77a2f256586cf0548d
Daniel TB <varig203@gmail.com>
2023-03-30 18:47:33 -0700

    Can't get start.py to work but it will work soon

---
+++ src/frontend.py
@@ -0,0 +1,23 @@
+# Frontend
+
+import gi, os
+gi.require_version('Gtk', "4.0")
+from gi.repository import Gtk
+
+# When the installer launches
+def on_activate(app):
+    # … create a new window…
+    win = Gtk.ApplicationWindow(application=app)
+    # … with a button in it…
+    btn = Gtk.Button(label='Lmao')
+    # … which closes the window when clicked
+    btn.connect('clicked', lambda x: win.close())
+    win.set_child(btn)
+    win.present()
+
+# Create a new application
+app = Gtk.Application(application_id='com.Sulphurium.ModpackInstaller')
+app.connect('activate', on_activate)
+
+# Run the application
+app.run(None)
---
+++ src/start.py
@@ -0,0 +1,9 @@
+import platform, windowsBackend, linuxBackend
+
+# Detecting operating system
+if platform.system == 'Linux':
+    linuxBackend.test()
+elif platform.system == 'Windows':
+    windowsBackend.test()
+else:
+    print("Could not find your system version")
