8f3a0ebfaae2d03c6a50fc945532b34ead93cab2
varig203 <varig203@gmail.com>
2023-03-31 11:04:11 -0700

    added install to linuxBackend

--- src/linuxBackend.py
+++ src/linuxBackend.py
@@ -1,6 +1,22 @@
-import os
+import os, requests
 
+sDownloads = True
+sUsrDir = os.path.expanduser('~')
+
+# Basically changes directory based on sDownloads
 def fnChangeDir():
-    sUsrDir = os.path.expanduser('~')
-    os.chdir(sUsrDir + str('/.minecraft'))
-
+    if sDownloads == False:
+        os.chdir(sUsrDir + str('/.minecraft'))
+    elif sDownloads == True:
+        os.chdir(sUsrDir + str('/Downloads'))
+    
+    
+def fnForgeInstall():
+    # Grabs the forge installer and runs it
+    sDownloads = True
+    fnChangeDir()
+    print('Download directory ' + (os.getcwd()))
+    
+    url = 'https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.2-43.2.8/forge-1.19.2-43.2.8-installer.jar'
+    r = requests.get(url, allow_redirects = True)
+    open('forge-installer.jar', 'wb').write(r.content)
--- src/start.py
+++ src/start.py
@@ -1,11 +1,15 @@
-import platform, windowsBackend, linuxBackend
+import platform, os, windowsBackend, linuxBackend
+
+sDownloads = True
+sUsrDir = os.path.expanduser('~')
 
 # Detecting operating system
 if platform.system() == "Linux":
     print("Linux has been detected")
-    linuxBackend.fnChangeDir()
+    linuxBackend.fnForgeInstall()
 elif platform.system() == "Windows":
     print("Windows has been detected")
     windowsBackend.test()
 else:
     print("Could not find your system version or your system is not supported")
+
